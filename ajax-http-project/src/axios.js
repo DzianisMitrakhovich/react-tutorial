52ad218e9aebc9bbf3090f0ac8064e7366340360
Dzianis_Mitrakhovich <dzianis_mitrakhovich@epam.com>
2020-10-01 18:25:41 +0300

    add axios pet project

---
+++ ajax-http-project/src/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const instance = axios.create(
+    {
+        baseURL : "https://jsonplaceholder.typicode.com"
+    }
+);
+
+instance.defaults.headers.common['Authorization'] = "AUTH HEADER FOR INSTANCE";
+
+export default instance;
